89a44bfe31cc1131ca1c6a7d95ff56f934c18136
FullAlternate <33181597+FullAlternate@users.noreply.github.com>
2018-02-20 19:33:56 +0100

    Add files via upload

--- oblig1/set.c
+++ oblig1/set.c
@@ -7,9 +7,18 @@ struct set {
     list_t *list;
 };
 
+struct set_iter{
+    list_iter_t *listIter;
+};
+
 
 set_t *set_create(cmpfunc_t cmpfunc) {
     set_t *s = malloc(sizeof(set_t));
+
+    if(s == NULL){
+        fatal_error("out of memory");
+    }
+
     s->list = list_create(cmpfunc);
 
     return s;
@@ -24,38 +33,157 @@ int set_size(set_t *set) {
     return list_size(set->list);
 }
 
-void set_add(set_t *set, void *elem){
+void set_add(set_t *set, void *elem){    
+    if(list_contains(set->list, elem) == 0){
     list_addlast(set->list, elem);
+    list_sort(set->list);
+    }
 }
 
 int set_contains(set_t *set, void *elem){
-    list_contains(set->list, elem);
+    return list_contains(set->list, elem);
 }
 
+
 set_t *set_union(set_t *a, set_t *b){
-    list_sort(a->list);
-    list_sort(b->list);
 
     set_t *c = set_create(compare_strings);
-
     list_iter_t *iterA = list_createiter(a->list);
     list_iter_t *iterB = list_createiter(b->list);
-    list_iter_t *iterC = list_createiter(c->list);
+    void *elemA;
+    void *elemB;
+    
+    while(list_hasnext(iterA) == 1){
+        elemA = list_next(iterA);
+
+        if(list_contains(c->list, elemA) == 0){
+            set_add(c, elemA);
+        } 
+    }
+
+    while(list_hasnext(iterB) == 1){
+        elemB = list_next(iterB);
+
+        if(list_contains(c->list, elemB) == 0){
+            set_add(c, elemB);
+        } 
+    }
+
+    list_sort(c->list);
+    return c;
+}
 
 
+/*
+ * Returns the intersection of the two given sets; the
+ * returned set contains all elements that are contained
+ * in both a and b.
+ */
+set_t *set_intersection(set_t *a, set_t *b){
+
+    set_t *c = set_create(compare_strings);
+    list_iter_t *iterA = list_createiter(a->list);
+    list_iter_t *iterB = list_createiter(b->list);
+    void *elemA;
+    void *elemB;
 
     while(list_hasnext(iterA) == 1){
+        elemA = list_next(iterA);
 
+        if(list_contains(c->list, elemA) == 0 && list_contains(b->list, elemA) == 1){
+            set_add(c, elemA);
+        } 
+    }
+
+    list_sort(c->list);
+    return c;
+}
+
+
+/*
+ * Returns the set difference of the two given sets; the
+ * returned set contains all elements that are contained
+ * in a and not in b.
+ */
+set_t *set_difference(set_t *a, set_t *b){
+
+    set_t *c = set_create(compare_strings);
+    list_iter_t *iterA = list_createiter(a->list);
+    void *elemA;
     
-    if(list_next(iterA) <= list_next(iterB)){
-        set_add(c, a->list->head->elem);
-        
+    while(list_hasnext(iterA) == 1){
+        elemA = list_next(iterA);
+
+        if(list_contains(c->list, elemA) == 0 && list_contains(b->list, elemA) == 0){
+            set_add(c, elemA);
+        } 
     }
 
-    else {
-        set_add(c, b->list->head->elem);
-        list_next(iterB);
+    list_sort(c->list);
+    return c;
+}
+
+
+/*
+ * Returns a copy of the given set.
+ */
+set_t *set_copy(set_t *set){
+    set_t *c = set_create(compare_strings);
+    list_iter_t *iter = list_createiter(set->list);
+    void *elem;
+    
+    while(list_hasnext(iter) == 1){
+        elem = list_next(iter);
+        set_add(c, elem);
+    }
+    
+    return c;
+}
+
+
+
+
+
+/*
+ * Creates a new set iterator for iterating over the given set.
+ */
+set_iter_t *set_createiter(set_t *set){
+    set_iter_t *iter = malloc(sizeof(set_iter_t));
+
+    if(iter == NULL){
+        fatal_error("out of memory");
     }
 
-    list_next(iterC);
-}    
+    iter->listIter = list_createiter(set->list);
+    return iter;
+}
+
+
+/*
+ * Destroys the given set iterator.
+ */
+void set_destroyiter(set_iter_t *iter){
+    list_destroyiter(iter->listIter);
+    free(iter);
+}
+
+
+/*
+ * Returns 0 if the given set iterator has reached the end of the
+ * set, or 1 otherwise.
+ */
+int set_hasnext(set_iter_t *iter){
+    return list_hasnext(iter->listIter);
+ 
+}
+
+
+/*
+ * Returns the next element in the sequence represented by the given
+ * set iterator.
+ */
+void *set_next(set_iter_t *iter){
+    
+    return list_next(iter->listIter);
+
+}
--- oblig1/spamfilter.c
+++ oblig1/spamfilter.c
@@ -53,6 +53,18 @@ static void printwords(char *prefix, set_t *words)
 	set_destroyiter(it);
 }
 
+char *spamcheck(int spamAmount){
+
+	char *check;
+	if(spamAmount > 0){
+		check = "SPAM";
+	} else {
+		check = "Not spam";
+	}
+
+	return check;
+}
+
 /*
  * Main entry point.
  */
@@ -69,5 +81,58 @@ int main(int argc, char **argv)
 	nonspamdir = argv[2];
 	maildir = argv[3];
 
+	set_t *s1 = tokenize("spam/spam1.txt");
+	set_t *s2 = tokenize("spam/spam2.txt");
+	set_t *s3 = tokenize("spam/spam3.txt");
+	set_t *s4 = tokenize("spam/spam4.txt");
+
+	s1 = set_intersection(s1, s2);
+	s3 = set_intersection(s3, s4);
+	s1 = set_intersection(s1, s3);
+
+	set_t *n1 = tokenize("nonspam/nonspam1.txt");
+	set_t *n2 = tokenize("nonspam/nonspam2.txt");
+	set_t *n3 = tokenize("nonspam/nonspam3.txt");
+	set_t *n4 = tokenize("nonspam/nonspam4.txt");
+
+	n1 = set_union(n1, n2);
+	n3 = set_union(n3, n4);
+	n1 = set_union(n1, n3);
+	set_t *combinedSet = set_difference(s1, n1);
+
+	char *mc1 = "mail/mail1.txt";
+	char *mc2 = "mail/mail2.txt";
+	char *mc3 = "mail/mail3.txt";
+	char *mc4 = "mail/mail4.txt";
+	char *mc5 = "mail/mail5.txt";
+
+	set_t *m1 = tokenize(mc1);
+	m1 = set_intersection(m1, combinedSet);
+
+	set_t *m2 = tokenize(mc2);
+	m2 = set_intersection(m2, combinedSet);
+
+	set_t *m3 = tokenize(mc3);
+	m3 = set_intersection(m3, combinedSet);
+
+	set_t *m4 = tokenize(mc4);
+	m4 = set_intersection(m4, combinedSet);
+	
+	set_t *m5 = tokenize(mc5);
+	m5 = set_intersection(m5, combinedSet);
+	
+	int sizem1 = set_size(m1);
+	int sizem2 = set_size(m2);
+	int sizem3 = set_size(m3);
+	int sizem4 = set_size(m4);
+	int sizem5 = set_size(m5);
+
+	printf("%s: %d spam word(s) -> %s\n", mc1, sizem1, spamcheck(sizem1));
+	printf("%s: %d spam word(s) -> %s\n", mc2, sizem2, spamcheck(sizem2));
+	printf("%s: %d spam word(s) -> %s\n", mc3, sizem3, spamcheck(sizem3));
+	printf("%s: %d spam word(s) -> %s\n", mc4, sizem4, spamcheck(sizem4));
+	printf("%s: %d spam word(s) -> %s\n", mc5, sizem5, spamcheck(sizem5));
+
     return 0;
 }
+
